00fe4f1e04d0dd97d00cf23af794f87ba526f0e1
lxh <lxh@qq.com>
2021-05-10 09:57:58 +0800

    第一次提交

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="color:red">
+      <h3>页面无法访问.....</h3><br>
+      <h3>NOT FOUND</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
